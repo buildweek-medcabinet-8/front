dfcf66513c49052a799dba0defebca39a6a1848e
elijah-atkins <elijahorama@gmail.com>
2020-06-22 00:40:40 -0600

    done for night

--- med-cabinet8/src/components/Recommendations.js
+++ med-cabinet8/src/components/Recommendations.js
@@ -2,41 +2,45 @@ import React from "react";
 import axiosWithAuth from "../utils/axiosWithAuth";
 
 class Recommendations extends React.Component {
-    state = {
-        recommendationList: [],
-        message: ''
-    }
+  state = {
+    recommendationList: [],
+    message: "",
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getData();
   }
 
   getData = () => {
     axiosWithAuth()
       .get("/profile/recommendations")
-      .then(res => {
-        console.log("ea: Recommendations.js getData results:", res.data);
+      .then((res) => {
+        //  console.log("ea: Recommendations.js getData results:", res.data);
         this.setState({
-            recommendationList: res.data.content,
-            message: res.data.message
-        })
+          recommendationList: res.data.content,
+          message: res.data.message,
+        });
       })
-      .catch(err =>
-        console.error("ea: Recommendations.js: getData: err.message: ", err.message)
+      .catch((err) =>
+        console.error(
+          "ea: Recommendations.js: getData: err.message: ",
+          err.message
+        )
       );
   };
   render() {
-  return (<div>
-      <ul>
-          <li>      {this.state.recommendationList.yourName}</li>
-<li>
-      {this.state.recommendationList.Strain}</li>
-      <li>{this.state.recommendationList.rating}</li>
-      <li>{this.state.recommendationList.type}</li>
-      <li>{this.state.message}</li>
-  </ul>
-  </div>);
+    return (
+      <div>
+        <ul>
+          <li> {this.state.recommendationList.yourName}</li>
+          <li>{this.state.recommendationList.Strain}</li>
+          <li>{this.state.recommendationList.rating}</li>
+          <li>{this.state.recommendationList.type}</li>
+          <li>{this.state.message}</li>
+        </ul>
+      </div>
+    );
   }
-};
+}
 
 export default Recommendations;
