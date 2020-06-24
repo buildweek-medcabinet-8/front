51c85334502ca49ce0eae8728c2e386e54d9ef42
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-06-24 17:16:10 -0400

    changed color of navbar text

--- med-cabinet8/src/components/Navbar.js
+++ med-cabinet8/src/components/Navbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Grid, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import NavMenu from './NavMenu';
 import LogoutSwitch from './Logout';
 
 const useStyles = makeStyles(() => ({
-	root: {
-		flexGrow: 1,
+	whiteText: {
+		color: '#fff',
 	},
 	title: {
 		flexGrow: 1,
@@ -22,23 +23,30 @@ function Navbar({ checked, setChecked }) {
 	const classes = useStyles();
 	return (
 		<React.Fragment>
-			<AppBar position='fixed' className={classes.appbar}>
+			<AppBar
+				position='fixed'
+				className={clsx(classes.appbar, classes.whiteText)}
+			>
 				<Toolbar>
-					{(checked === true) ? <NavMenu /> : null}
+					{checked === true ? <NavMenu /> : null}
 					<Typography variant='h5' component='span' className={classes.title}>
 						Med-Cabinet
 					</Typography>
-					{(checked) ?<LogoutSwitch checked={checked} setChecked={setChecked} /> : null}
-					{(!checked) ? 				<Grid item>
-					<Button
-						variant='outlined'
-						color='secondary'
-						component={Link}
-						to='/register'
-					>
-						Sign Up!
-					</Button>
-				</Grid> : null}
+					{checked ? (
+						<LogoutSwitch checked={checked} setChecked={setChecked} />
+					) : null}
+					{!checked ? (
+						<Grid item>
+							<Button
+								variant='outlined'
+								color='secondary'
+								component={Link}
+								to='/register'
+							>
+								Sign Up!
+							</Button>
+						</Grid>
+					) : null}
 				</Toolbar>
 			</AppBar>
 			<Toolbar />
