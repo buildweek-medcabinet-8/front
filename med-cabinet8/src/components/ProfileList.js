8d6c931207f8c36df02bc086fd27aeb6d0b0eb7c
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-06-27 18:12:27 -0400

    added help text

--- med-cabinet8/src/components/ProfileList.js
+++ med-cabinet8/src/components/ProfileList.js
@@ -12,7 +12,7 @@ import {
 	Dialog,
 } from '@material-ui/core';
 import TabPanel from './TabPanel';
-
+import theme from '../theme';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
@@ -21,6 +21,18 @@ const useStyles = makeStyles({
 	},
 	tabs: {
 		width: '100%',
+	},
+	works: {
+		marginBottom: '2rem',
+		width: '50%',
+		textAlign: 'center',
+
+		[theme.breakpoints.down('md')]: {
+			width: '90%',
+		},
+	},
+	worksText: {
+		padding: '2rem',
 	},
 });
 
@@ -44,76 +56,86 @@ const ProfileList = ({ profileObj }) => {
 	};
 
 	return (
-		<Grid container direction='column'>
-			<Grid item container>
-				<Grid item>
-					{profile.length === 0 ? (
-						<Typography variant='subtitle1'>
-							<p className="intro">You don't have any profiles yet click add profile to get started</p>
-						</Typography>
-					) : null}
-				</Grid>
+		<Grid container direction='column' alignItems='center' justify='center'>
+			<Grid item>
+				{profile.length === 0 ? (
+					<Typography variant='subtitle1'>
+						You don't have any profiles yet click add profile to get started
+					</Typography>
+				) : null}
+			</Grid>
 
-				<Grid
-					item
-					container
-					direction='row'
-					alignItems='center'
-					justify='space-around'
-					className={classes.buttons}
-				>
-					<Grid item>
-						<Button
-							variant='contained'
-							color='secondary'
-							onClick={() => setEditModalOpen(true)}
-						>
-							Edit Profile
-						</Button>
-					</Grid>
-					<Grid item>
-						<Button
-							variant='contained'
-							color='secondary'
-							onClick={() => setAddModalOpen(true)}
-						>
-							Add Profile
-						</Button>
-					</Grid>
+			<Grid item className={classes.works}>
+				<Paper className={classes.worksText}>
+					<Typography variant='p' color='initial' align='center'>
+						To get started, click on "Add Profile", and create a profile by
+						giving it a name, providing at least 1 desired flavor and 1 desired
+						effect, and a brief description, and we will return a list of our 5
+						best recommendations that meet your needs. You can change any
+						profile at any time by choosing "Update Profile".
+					</Typography>
+				</Paper>
+			</Grid>
+
+			<Grid
+				item
+				container
+				direction='row'
+				alignItems='center'
+				justify='space-around'
+				className={classes.buttons}
+			>
+				<Grid item>
+					<Button
+						variant='contained'
+						color='secondary'
+						onClick={() => setEditModalOpen(true)}
+					>
+						Edit Profile
+					</Button>
 				</Grid>
-				<Grid item className={classes.tabs}>
-					<Paper>
-						<Tabs
-							value={value}
-							onChange={handleChange}
-							aria-label='simple tabs example'
-							centered
-							variant='fullWidth'
-						>
-							{profile.map((item, index) => {
-								return <Tab key={index} label={item[0]} />;
-							})}
-						</Tabs>
-					</Paper>
-					<Grid item>
+				<Grid item>
+					<Button
+						variant='contained'
+						color='secondary'
+						onClick={() => setAddModalOpen(true)}
+					>
+						Add Profile
+					</Button>
+				</Grid>
+			</Grid>
+			<Grid item className={classes.tabs}>
+				<Paper>
+					<Tabs
+						value={value}
+						onChange={handleChange}
+						aria-label='simple tabs example'
+						centered
+						variant='fullWidth'
+					>
 						{profile.map((item, index) => {
-							return (
-								<TabPanel value={value} index={index}>
-									<Recommendations object={profile[index]} />
-									<Dialog open={editModalOpen} onClose={handleEditModalClose}>
-										<UpdateStrain
-											object={profile[index]}
-											profile={profile}
-											setProfile={setProfile}
-											id={index}
-											setDialogOpen={setEditModalOpen}
-											handleClose={handleEditModalClose}
-										/>
-									</Dialog>
-								</TabPanel>
-							);
+							return <Tab key={index} label={item[0]} />;
 						})}
-					</Grid>
+					</Tabs>
+				</Paper>
+				<Grid item>
+					{profile.map((item, index) => {
+						return (
+							<TabPanel value={value} index={index}>
+								<Recommendations object={profile[index]} />
+								<Dialog open={editModalOpen} onClose={handleEditModalClose}>
+									<UpdateStrain
+										object={profile[index]}
+										profile={profile}
+										setProfile={setProfile}
+										id={index}
+										setDialogOpen={setEditModalOpen}
+										handleClose={handleEditModalClose}
+									/>
+								</Dialog>
+							</TabPanel>
+						);
+					})}
 				</Grid>
 			</Grid>
 
